17412469c1ad6ea665b5df7db0d7a7199f82e315
Yozu Liang <yozukitty@gmail.com>
2015-06-05 16:18:30 -0700

    AdminEntity added

---
+++ phase3/AnimeWebsite/Entities/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Admin
+ *
+ * @author yozubear
+ */
+class Admin {
+    //put your code here
+    public $admin_name;
+    public $password;
+    
+    function _construct($admin_name,$password){
+        $this->admin_name = $admin_name;
+        $this->password = $password;
+    }
+    
+}
