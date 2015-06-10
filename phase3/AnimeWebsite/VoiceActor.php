cdf87f3d1e11fab1c080de8509a834aaffd3818a
Shehryar Awan <j4a8@deas.ugrad.cs.ubc.ca>
2015-06-09 18:42:04 -0700

    Hard coded entries for sample

--- phase3/AnimeWebsite/VoiceActor.php
+++ phase3/AnimeWebsite/VoiceActor.php
@@ -2,14 +2,18 @@
 $title = "Voice Actor";
 $content = '
        
-        <h1>Find Voice Actor</h1>
-         <div id="sidebar">
-                <img src="Images/VoiceActor/Mikako.jpg" class="imgRight" style="float:left;width:225px;height:350px;" />
+        <h1>Find Voice Actor</h1> 
+    
+        <div><img src="Images/VoiceActor/Mikako.jpg" hspace="20"><br>
+        <FONT COLOR="Maroon">Name:</FONT> Mikako Takahashi<br>
+        <FONT COLOR="Maroon">Birthday:</FONT> May 29 1980<br>
+        <FONT COLOR="Maroon">Gender:</FONT> Female<br>
+        <FONT COLOR="Maroon">Biography:</FONT> Mikako Takahashi born May 29, 1980 in Chiba Prefecture is a Japanese voice actress and J-pop singer. As the Excel Saga character Mikako Hyatt, she forms one half of the voice acting duo The Excel Girls She is employed by Im Enterprise. <br>    
             </div>
-        <p>Put Actor Here</p>
-
-          
-
+             
+       
+        
+        
 
 ';
 include 'Template.php';
