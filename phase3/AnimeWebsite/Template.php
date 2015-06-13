78a25e337e15ec1d28d4cb706c49cc9893a82ab5
Weining Hu <weininghu1012@gmail.com>
2015-06-13 11:02:58 -0700

    add ui for register part

--- phase3/AnimeWebsite/Template.php
+++ phase3/AnimeWebsite/Template.php
@@ -4,43 +4,10 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-        <script src="Styles/bootstrap.min.js"></script>
-        <link href="Styles/bootstrap.min.css" rel="stylesheet" media="screen">
+        
     </head>
     <body>
-        <div class="navbar navbar-fixed-top navbar-inverse">
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand">Welcome to Animanga Website!</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav pull-right">
-                            <li><a href="register.php">Register</a></li>
-                            <li class="divider-vertical"></li>
-                            <li class="dropdown">
-                                <a class="dropdown-toggle" href="#" data-toggle="dropdown">Log In <strong class="caret"></strong></a>
-                                <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
-                                    <form action="index.php" method="post"> 
-                                        Username:<br /> 
-                                        <input type="text" name="username" value="<?php echo $submitted_username; ?>" /> 
-                                        <br /><br /> 
-                                        Password:<br /> 
-                                        <input type="password" name="password" value="" /> 
-                                        <br /><br /> 
-                                        <input type="submit" class="btn btn-info" value="Login" /> 
-                                    </form> 
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+      
 
         <div id="wrapper">
             <div id="banner">   
@@ -50,7 +17,7 @@
 
                 
                 <a style="color:whitesmoke; font-size:25px" href="Login.php">Login</a>
-                    <a style="color:whitesmoke; font-size:25px" href="#">Register</a>
+                <a style="color:whitesmoke; font-size:25px" href="Register.php">Register</a>
 
             </div>
 
